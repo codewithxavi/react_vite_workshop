59cad58389e4fc2d9adebd08b865189b6a779ebb
xavicoder <javieradrian.it@gmail.com>
2022-04-28 00:44:11 +0200

    Displaying missions

---
+++ src/components/LaunchItem.jsx
@@ -0,0 +1,25 @@
+import { HiCalendar } from "react-icons/hi";
+import { Box, Text, Spacer, Tag, Flex, Button, Icon } from "@chakra-ui/react";
+
+export function LaunchItem(launch) {
+  return (
+    <Box bg="gray.200" p={4} m={4} borderRadius="lg">
+      <Box display="flex">
+        <Text fontSize="2xl">
+          Mission <strong>{launch.mission_name}</strong> ({launch.launch_year})
+        </Text>
+        <Spacer />
+        <Tag p={2} colorScheme={launch.launch_success ? "green" : "red"}>
+          {launch.launch_success ? "Success" : "Failure"}
+        </Tag>
+      </Box>
+      <Flex align="center">
+        <Icon as={HiCalendar} color="gray" />
+        <Text fontSize="sm" ml="1" color="gray.400">
+          {launch.launch_date_local.split("T")[0]}
+        </Text>
+      </Flex>
+      <Button colorScheme="purple"> More details</Button>
+    </Box>
+  );
+}
